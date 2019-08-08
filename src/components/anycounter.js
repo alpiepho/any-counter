3aa211bb37402a745ab53d712193e17780f90697
al <alpiepho@yahoo.com>
2019-08-08 08:17:08 -0600

    starting any-counter PWA application

---
+++ src/components/anycounter.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Button from '@material-ui/core/Button';
+
+const AnyCounter = () => (
+  <div>
+    <div>
+      Any-Counter
+    </div>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  </div>
+)
+
+export default AnyCounter
